9827f01453925d55f3a2dfa6bab25299ec2253ac
Rob Galanakis <rob.galanakis@gmail.com>
2014-07-30 14:58:52 -0500

    double to single quotes.

--- minimaestro/theme_settings.py
+++ minimaestro/theme_settings.py
@@ -1,6 +1,6 @@
-title = "Mini Maestro - Kids Music Game"
-branding = "Mini Maestro"
-copyright_html = "Copyright &copy; Brian Mumm and Rob Galanakis 2014"
+title = 'Mini Maestro - Kids Music Game'
+branding = 'Mini Maestro'
+copyright_html = 'Copyright &copy; Brian Mumm and Rob Galanakis 2014'
 
 # You're the conductor!
 # Choose up to 6 instruments to put in the orchestra pit and
@@ -14,72 +14,72 @@ copyright_html = "Copyright &copy; Brian Mumm and Rob Galanakis 2014"
 # - Professionally recorded sounds
 # - Included playbill teaches composers, writers and lyrics
 # - Watch the video!
-jumbo_title = "Become a Maestro!"
-jumbo_text = ("Create beautiful music by placing up to 6 instruments into"
-              "the orchestra pit."
-              "Choose new instruments and experience the "
-              "musical transformation.")
+jumbo_title = 'Become a Maestro!'
+jumbo_text = ('Create beautiful music by placing up to 6 instruments into'
+              'the orchestra pit.'
+              'Choose new instruments and experience the '
+              'musical transformation.')
 
-link_itunes = "https://itunes.apple.com/us/app/safari-rush/id825143127"
+link_itunes = 'https://itunes.apple.com/us/app/safari-rush/id825143127'
 
-benefits_title = "Features"
+benefits_title = 'Features'
 benefits = [
     {
-        "text": "Accessible and educational for children of all ages."
+        'text': 'Accessible and educational for children of all ages.'
     },
     {
-        "text": "Arrange 11 instruments, including clarinet, flute, "
-                "tuba, trumpet, french horn, "
-                "drums, cymbals, xylophone, piano, "
-                "harp, and violin."
+        'text': 'Arrange 11 instruments, including clarinet, flute, '
+                'tuba, trumpet, french horn, '
+                'drums, cymbals, xylophone, piano, '
+                'harp, and violin.'
     },
     {
-        "text": "Experience 7 classical symphonies from composers including "
-                "Beethoven, Vivaldi, Tchaikovsky, and Fučík."
+        'text': 'Experience 7 classical symphonies from composers including '
+                'Beethoven, Vivaldi, Tchaikovsky, and Fučík.'
     },
     {
-        "text": "Learn the different sections of an orchestra."
+        'text': 'Learn the different sections of an orchestra.'
     },
 ]
 
-tour_title = "Screenshots and Videos"
+tour_title = 'Screenshots and Videos'
 tour_items = [
     {
-        "caption_html": "Amazing photos provided by <a href=\"http://unsplash.com\">unsplash.com</a>",
-        "src": "static/img/stock1.jpg"
+        'caption_html': 'Amazing photos provided by <a href=\'http://unsplash.com\'>unsplash.com</a>',
+        'src': 'static/img/stock1.jpg'
     },
     {
-        "caption_html": "Boat in Water",
-        "src": "static/img/stock2.jpg"
+        'caption_html': 'Boat in Water',
+        'src': 'static/img/stock2.jpg'
     },
     {
-        "caption_html": "Millennium Bridge in London",
-        "src": "static/img/stock3.jpg"
+        'caption_html': 'Millennium Bridge in London',
+        'src': 'static/img/stock3.jpg'
     },
     {
-        "caption_html": "Blurs",
-        "src": "static/img/stock4.jpg"
+        'caption_html': 'Blurs',
+        'src': 'static/img/stock4.jpg'
     },
     {
-        "caption_html": "Santorini Greece",
-        "src": "static/img/stock5.jpg"
+        'caption_html': 'Santorini Greece',
+        'src': 'static/img/stock5.jpg'
     }
 ]
 
-support_title = "Support"
+support_title = 'Support'
 support_html = (
     "If you have a problem, request, or suggestion, don't hesitate to "
-    "<a href=\"mailto:support@minimaestrogame.com?Subject=Support Request\" target=\"_top\">email us</a>!<br/>"
-    "You can also create an issue on our public issue tracker <a href=\"https://github.com/rgalanakis/minimaestro-public/issues\">here</a>."
+    '<a href="mailto:support@minimaestrogame.com?Subject=Support Request" target="_top">email us</a>!<br/>'
+    'You can also create an issue on our public issue tracker <a href="https://github.com/rgalanakis/minimaestro-public/issues">here</a>.'
 )
 
-about_title = "About Mini Maestro"
+about_title = 'About Mini Maestro'
 about_paragraphs = [
-    ("Mini Maestro is a game about enjoying music. "
-     "It is inspired by a toy my mother bought for my first son. "
-     "I saw how much fun he got out of it: exploring instruments, "
-     "learning classical songs, and just dancing like a madman. "
-     "Mini Maestro aspires to deliver this same joy but in a digital form."),
-    ("This game was created by Brian Mumm and Rob Galanakis. "
-     "I hope you enjoy playing Mini Maestro as much as we enjoyed making it!")
+    ('Mini Maestro is a game about enjoying music. '
+     'It is inspired by a toy my mother bought for my first son. '
+     'I saw how much fun he got out of it: exploring instruments, '
+     'learning classical songs, and just dancing like a madman. '
+     'Mini Maestro aspires to deliver this same joy but in a digital form.'),
+    ('This game was created by Brian Mumm and Rob Galanakis. '
+     'I hope you enjoy playing Mini Maestro as much as we enjoyed making it!')
 ]
